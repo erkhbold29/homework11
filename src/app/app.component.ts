468ce7987beeabe518a9b29b8ab63a2ecfb8cf0c
erkhbold29 <erkhbold29@gmail.com>
2017-02-02 17:05:13 -0600

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { CounterComponent } from './counterComponent/counter.component';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,5 @@ import { CounterComponent } from './counterComponent/counter.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = CounterComponent;
+ title = 'app works!';
 }
